fdd43bd376a19b801b0810b79f39479227e530ac
oscarcamilo <oscar.ca@hotmail.com>
2022-09-13 10:21:23 -0500

    Creacion page Impresora

---
+++ Impresora3d.App.Fontend/Pages/Impresora/List.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Impresora3d.App.Fontend.Pages
+{
+    public class ListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
