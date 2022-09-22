28ae8e462d21c7f7aa881ba1422517bba82ad5fb
oscarcamilo <oscar.ca@hotmail.com>
2022-09-22 17:34:56 -0500

    Validacion Persona

--- Impresora3d.App.Dominio/Entidades/Persona.cs
+++ Impresora3d.App.Dominio/Entidades/Persona.cs
@@ -6,21 +6,22 @@ namespace Impresora3d.App.Dominio
 {
     public class Persona{
         public int Id { get; set;}
-       // [Required, Range(1,100000000000)]
+        [Required, Range(1,100000000000)]
         public int Identificacion { get; set;}
-        //[Required, StringLength(50)]
+        [Required, StringLength(50)]
         public string Nombre { get; set;}
-        //[Required, StringLength(50)]
+        [Required, StringLength(50)]
         public string Apellidos { get; set;}
-        //[Required, StringLength(50)]
+        [Required, StringLength(50)]
         public string NumeroTelefono { get; set;}
         public DateTime FechaNacimiento { get; set;}
-        //[Required, StringLength(50)]
+        [Required, StringLength(50)]
         public string Correo { get; set;}
-        //[Required, StringLength(50)]
+        [Required, StringLength(50)]
         public string Direccion {get;set;}
-        //[Required, StringLength(50)]
+        [Required, StringLength(50)]
         public string NivelEstudio {get;set;}
+        [Required, StringLength(50)]
         public string Discriminator { get; set;} 
 
     }
